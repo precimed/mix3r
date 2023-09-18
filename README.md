9aa3a0c08485673c7e3bb04063554a7d0d9b197e
interCM <inter.cm@outlook.com>
2023-09-18 22:41:17 +0200

    R script to produce Euler diagrams.

--- README.md
+++ README.md
@@ -22,3 +22,25 @@ tar -zxf mix3r.tar.gz -C mix3r/
 source mix3r/bin/activate
 conda-unpack
 ```
+
+### To run on TSD:
+1. Prepare config files (you may use `config_scz_adhd_mig_aug22_1.json` as template) for multiple (e.g. 16) runs with different variant subsets. You may do it by changing `"rand_prune_seed"` parameter in the config file, e.g. set `"rand_prune_seed" : i` for the i-th run (also don't forget to use different output files for all runs, e.g. for the i-th run set `"out" : "my_analysis_run_i.json"`).
+2. Submit jobs for all configs to cluster:
+```
+for i in {1..16}; do sbatch run_slurm_int.sh path/to/your_config_${i}.json; done
+```
+3. When all runs are finished, produce a table with estimated model parameters for all runs:
+```
+pyhton exrtact_p.py --input my_analysis_run_*.json --out my_analysis_all_runs
+```
+`my_analysis_all_runs.parameters.csv` file will be created.
+You can also produce a table for a selection of runs:
+```
+pyhton exrtact_p.py --input my_analysis_run_1.json my_analysis_run_3.json my_analysis_run_7.json --out my_analysis_selected_runs
+```
+4. Plot "median" Euler diagram:
+```
+Rscript make_euler.r my_analysis_all_runs.parameters.csv my_analysis.euler.png "Trait 1" "Trait 2" "Trait 3"  
+```
+Where the first argument is a path to the file with model parameters produced with `extract_p.py` script, the second argument is output file name and the last three arguments are trait labels to be used in the Euler diagram.
+
---
+++ make_euler.r
@@ -0,0 +1,53 @@
+library(eulerr)
+library(data.table)
+
+args <- commandArgs(trailingOnly = TRUE)
+# args[1] : path to *.parameters.csv file produced by extract_p.py; "bdbiobank_bdclinical_bdselfreport_sep12.parameters.csv"
+# args[2] : path to output file; "bdbiobank_bdclinical_bdselfreport_sep12.parameters.euler.png"
+# args[3], args[4], args[5] : labels for trait 1, trait 2 and trait 3 in Euler diagram; "BD biobank" "BD clinical" "BD self-repotr"
+# args[6], args[7], args[8] : hex colors for trait 1, trait 2 and trait 3 in Euler diagram; "#77AADD" "#EE8866" "#EEDD88"
+
+if (length(args) < 5) {
+  stop("Missing arguments!")
+} else {
+  fname <- args[1]
+  outf <- args[2]
+  labels <- c(args[3], args[4], args[5])
+  if (length(args) == 8) {
+    colors <- c(args[6], args[7], args[8])
+  } else {
+    colors <- c("#77AADD", "#EE8866", "#EEDD88")
+  }
+} 
+
+df <- fread(fname)
+i_best_run = which.min(df$rank_p_proportion_deviation_from_median)
+best = df[i_best_run]
+
+acceptable_negative_gap = -1E-6
+p1 <- best$p_1 - best$p_12 - best$p_13 + best$p_123
+if (p1<0 & p1>acceptable_negative_gap) {p1 <- 0}
+p2 <- best$p_2 - best$p_12 - best$p_23 + best$p_123
+if (p2<0 & p2>acceptable_negative_gap) {p2 <- 0}
+p3 <- best$p_3 - best$p_13 - best$p_23 + best$p_123
+if (p3<0 & p3>acceptable_negative_gap) {p3 <- 0}
+p12 <- best$p_12 - best$p_123
+if (p12<0 & p12>acceptable_negative_gap) {p12 <- 0}
+p13 <- best$p_13 - best$p_123
+if (p13<0 & p13>acceptable_negative_gap) {p13 <- 0}
+p23 <- best$p_23 - best$p_123
+if (p23<0 & p23>acceptable_negative_gap) {p23 <- 0}
+p123 <- best$p_123
+if (p123<0 & p1>acceptable_negative_gap) {p123 <- 0}
+vec2plot = c("1"=p1,"2"=p2,"3"=p3,"1&2"=p12,"1&3"=p13,"2&3"=p23,"1&2&3"=p123)
+
+fit <- euler(vec2plot, input="disjoint", shape="ellipse")
+
+png(filename=outf)
+plot(fit,
+     fills = list(fill=colors, alpha=1),
+     labels = list(labels=labels, col="black", fontsize=18),
+     edges = list(col="white", lex=2),
+     quantities = list(type="percent", fontsize=18))
+supressed_output = dev.off()
+cat(paste("Figure saved to:", outf, "\n"))
